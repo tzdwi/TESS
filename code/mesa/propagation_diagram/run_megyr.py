45c8a2512f0b7f2570f952ea36153bbc1dd5eb94
Trevor Dorn-Wallenstein <trevorzaylen@gmail.com>
2020-05-11 13:04:02 -0700

    MESA stuff

---
+++ code/mesa/propagation_diagram/run_megyr.py
@@ -0,0 +1,41 @@
+import megyr
+import os
+
+def run_megyr(masses,outdir):
+    megyr.run({
+        "input": {
+            "mesa_configs": ["inlist.mustache"]
+        },
+        "output": {
+            "mesa_profile_summary_file": "mesa_profile_attributes.csv",
+            "output_dir" : outdir
+        },
+        "stages": {
+            "mesa_params": {
+                "initial_mass": masses
+            }
+        }
+    })
+    
+if __name__ == '__main__':
+    outdir1 = 'out'
+    masses = [22,24,27,30] #[15,16,17,18,19,20,21,23,25,26,28,29] #
+    run_megyr(masses,outdir1)
+    os.rename("inlist.mustache","inlist_pre.mustache")
+    os.rename("inlist_post.mustache","inlist.mustache")
+    os.remove("completed_tasks.csv")
+    outdir2 = 'out_post'
+    try:
+        os.mkdir(outdir2)
+    except FileExistsError:
+        pass
+    for m in masses:
+        try:
+            os.mkdir(outdir2+f'/mesa_initial_mass_{m}__')
+        except FileExistsError:
+            pass
+        os.system(f'cp {outdir1}/mesa_initial_mass_{m}__/{m}M_at_ZAMS.mod {outdir2}/mesa_initial_mass_{m}__/{m}M_at_ZAMS.mod')
+        os.system(f'cp profile_columns.list {outdir2}/mesa_initial_mass_{m}__/profile_columns.list')
+    run_megyr(masses,outdir2)
+    os.rename("inlist.mustache","inlist_post.mustache")
+    os.rename("inlist_pre.mustache","inlist.mustache")
