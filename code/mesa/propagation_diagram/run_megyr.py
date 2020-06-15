9366e4250c2fca23e69d3f9ae4231d9392ce44b0
Trevor Dorn-Wallenstein <trevorzaylen@gmail.com>
2020-06-15 12:19:46 -0700

    final version for submission

--- code/mesa/propagation_diagram/run_megyr.py
+++ code/mesa/propagation_diagram/run_megyr.py
@@ -19,11 +19,11 @@ def run_megyr(masses,outdir):
     
 if __name__ == '__main__':
     outdir1 = 'out'
-    masses = [22,24,27,30] #[15,16,17,18,19,20,21,23,25,26,28,29] #
-    run_megyr(masses,outdir1)
+    masses = [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+    #run_megyr(masses,outdir1) #already have PMS models
     os.rename("inlist.mustache","inlist_pre.mustache")
     os.rename("inlist_post.mustache","inlist.mustache")
-    os.remove("completed_tasks.csv")
+    #os.remove("completed_tasks.csv")
     outdir2 = 'out_post'
     try:
         os.mkdir(outdir2)
